52cc1152b6b8476edf9c8d4325e875b9a131af15
Rafal_Kasperkiewicz <kspr@viessmann.net>
2024-07-19 13:00:06 +0200

    Add style in main.css. Add footer and header to project as a partial ejs files

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) =>{
+    res.render("index.ejs");
+});
 app.listen(port, ()=>{
-console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
