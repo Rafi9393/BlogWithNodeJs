f72729f8a781f28c06b3bc5a19b20171280f5707
Rafal_Kasperkiewicz <kspr@viessmann.net>
2024-07-19 09:59:39 +0200

    Add partial footer.html and set static file path

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from "express";
 const port =3000;
 const app = express();
 
+app.use(express.static("public"));
+
 app.listen(port, ()=>{
 console.log(`App listening on port ${port}`);
 });
