4f8b19304669ea4996a4a8c61347a34e186b0924
Rafal_Kasperkiewicz <kspr@viessmann.net>
2024-07-23 09:55:07 +0200

    Proper post view on view.ejs, add also intrested posr feature and post koments proper display

--- index.js
+++ index.js
@@ -27,6 +27,20 @@ app.post("/submit-form", (req, res) => {
     res.render("index.ejs", {postData: postData});
 });
 
+app.get('/read-post/:id', (req, res) => {
+    const postId = req.params.id;
+    const post = postData.find(p => p.id === postId);
+
+    if (post) {
+        res.render('view.ejs', { post: post, postData: postData , commentData: commentData});
+    } else {
+        res.status(404).send('Post not found');
+    }
+});
+
+
+
+
 app.listen(port, ()=>{
     console.log(`App listening on port ${port}`);
 });
@@ -37,20 +51,28 @@ app.listen(port, ()=>{
 
 let postData = [
      {
-    id: 1,
+    id: "c8adb598-6492-4cdf-baae-5a8096504086",
     imagePath: "imge1.jpg",
     postTitle: "Test spieniaczy do mleka - jaki wybrać?",
-    postContent: "Ręczny czy elektryczny? I jak ich używać? Tutaj zawiera się kolejna część tekstu tego posta. Tekst ten ma na celu wypełnienie strony tak by można było zobacyzć większy kafel. Tekstu było za mało, więc autor postanowił dopisać coniecco poniżej, i to jest ta cześć która właśnie została dopisana. Szybko dopisana..."
+    postContent: "Ręczny czy elektryczny? I jak ich używać? Tutaj zawiera się kolejna część tekstu tego posta. Tekst ten ma na celu wypełnienie strony tak by można było zobacyzć większy kafel. Tekstu było za mało, więc autor postanowił dopisać coniecco poniżej, i to jest ta cześć która właśnie została dopisana. Szybko dopisana"
 },
 {
-    id: 2,
+    id: "c8adb532-6492-4cdf-baae-5a8096504086",
     imagePath: "imge2.jpg",
     postTitle: "Kolejny post związany z tematyką bloga",
-    postContent: "Ręczny czy elektryczny? I jak ich używać? Tutaj zawiera się kolejna część tekstu tego posta. Tekst ten ma na celu wypełnienie strony tak by można było zobacyzć większy kafel. Tekstu było za mało, więc autor postanowił dopisać coniecco poniżej, i to jest ta cześć która właśnie została dopisana. Szybko dopisana..."
+    postContent: "Ręczny czy elektryczny? I jak ich używać? Tutaj zawiera się kolejna część tekstu tego posta. Tekst ten ma na celu wypełnienie strony tak by można było zobacyzć większy kafel. Tekstu było za mało, więc autor postanowił dopisać coniecco poniżej, i to jest ta cześć która właśnie została dopisana. Szybko dopisana"
 },
 {
-    id: 3,
+    id: "c8adb598-6492-4cdf-baae-5a8096512856",
     imagePath: "imge3.jpg",
     postTitle: "To jak na razie ostatni blog związany z tematyką bloga",
-    postContent: "Ręczny czy elektryczny? I jak ich używać? Tutaj zawiera się kolejna część tekstu tego posta. Tekst ten ma na celu wypełnienie strony tak by można było zobacyzć większy kafel. Tekstu było za mało, więc autor postanowił dopisać coniecco poniżej, i to jest ta cześć która właśnie została dopisana. Szybko dopisana..."
+    postContent: "Ręczny czy elektryczny? I jak ich używać? Tutaj zawiera się kolejna część tekstu tego posta. Tekst ten ma na celu wypełnienie strony tak by można było zobacyzć większy kafel. Tekstu było za mało, więc autor postanowił dopisać coniecco poniżej, i to jest ta cześć która właśnie została dopisana. Szybko dopisana"
+}];
+
+let commentData = [{
+    postId: "c8adb598-6492-4cdf-baae-5a8096512856",
+    commentDate: "23-07-2024 12:35",
+    commentContent: "Bardzo przydatny artykuł! Zastanawiałam się nad zakupem spieniacza i teraz wiem, który wybrać. Dzięki!",
+    commentName: "Anna"
+    
 }];
